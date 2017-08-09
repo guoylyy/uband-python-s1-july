ea124199f4df80fc9c0e6611789dadeb34a65fee
guohailu <1948492265@qq.com>
2017-08-09 16:41:24 +0800

    add homework files

---
+++ homework/A16042/check-in01/day1-homework.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @author: Guoshushu
+
+
+# For beginner
+# 1. variable - num,str,boolean
+# 2. if
+# 3. > < >= <= == 
+# 4. print
+def main():
+	who = '锅蜀黎的老妈'
+	good_price = 6  #小贩的价格
+	good_description = "西双版纳大白菜"  #小贩的招牌
+
+	is_cheap = False #是否便宜
+	reasonable_price = 5 #老妈能接受的最高价格
+	buy_amount = 2 #准备买2斤
+
+	#start
+	print"%s上街看到了%s,卖%d元/斤" % (who,good_description,good_price)
+
+	if good_price <= reasonable_price:
+		print '她认为便宜'
+		is_cheap = True
+		print '她买了 %d 斤' % (buy-amount)
+	else:
+		print '她认为贵了'
+		is_cheap = False 
+		print '她并没有买,扬长而去'
+
+# run function
+if __name__ == '__main__':
+  main()
+
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @author: Guoshushu
+
+
+# For beginner
+# 1. variable - num,str,boolean
+# 2. if
+# 3. > < >= <= == 
+# 4. print
+def main():
+	who = '我老妈'
+	good_price = 6  #小贩的价格
+	good_description = "西双版纳大白菜"  #小贩的招牌
+
+	is_cheap = False #是否便宜
+	reasonable_price = 5 #老妈能接受的最高价格
+	buy_amount = 2 #准备买2斤
+
+	#start
+	print"%s上街看到了%s,卖%d元/斤" % (who,good_description,good_price)
+
+	if good_price <= reasonable_price:
+		print '她认为便宜'
+		is_cheap = True
+		print '她买了 %d 斤' % (buy-amount)
+	else:
+		print '她认为贵了'
+		is_cheap = False 
+		print '她并没有买,扬长而去'
+
+# run function
+if __name__ == '__main__':
+  main()
+
+
+
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @author: Guoshushu
+
+
+# For beginner
+# 1. variable - num,str,boolean
+# 2. if
+# 3. > < >= <= == 
+# 4. print
+def main():
+	who = '我老妈'
+	good_price = 6  #小贩的价格
+	good_description = "西双版纳大白菜"  #小贩的招牌
+
+	is_cheap = False #是否便宜
+	reasonable_price = 7 #老妈能接受的最高价格
+	buy_amount = 2 #准备买2斤
+
+	#start
+	print"%s上街看到了%s,卖%d元/斤" % (who,good_description,good_price)
+
+	if good_price <= reasonable_price:
+		print '她认为便宜'
+		is_cheap = True
+		print '她买了 %d 斤' % (buy_amount)
+	else:
+		print '她认为贵了'
+		is_cheap = False 
+		print '她并没有买,扬长而去'
+
+# run function
+if __name__ == '__main__':
+  main()
+
+
+
+#变量：例如num,str,bool等自身可能发生改变而引起结果发生变化的量。
+#if:表示可能出现的事件不同的发展方向引起不同的结果的条件性语句。
+#<= >=:数字变量的变化范围。
